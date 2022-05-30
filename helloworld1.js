686642582085278c5e6ea0328f4c620be60b5348
Satish Thakur <satishthakur5534@gmail.com>
2022-05-30 16:01:59 +0530

    hellow world 1 commit

---
+++ helloworld1.js
@@ -0,0 +1,47 @@
+var day = 0;
+switch (day) {
+    case 0:
+        console.log("today is monday");
+        break;
+
+    case 1:
+        console.log("today is tue");
+        break;
+
+    case 2:
+        console.log("today is wed");
+        break;
+
+    case 3:
+        console.log.write("today is thursday");
+        break;
+
+    case 4:
+        console.log("today is friday");
+        break;
+
+    case 5:
+        console.log("today is saturday");
+        break;
+
+    case 6:
+        console.log("today is sunday");
+        break;
+
+        default:
+        console.log("please write the invalid day");
+}
+// var day = 0;
+// switch (day) {
+//     case 0:
+//         console.log("today is monday");
+//         break;
+
+//     case 1:
+//     console.log("today is tue");
+//         break;
+
+    
+//         default:
+//         console.log("please write the invalid day");
+// }
